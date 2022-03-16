eb3184d8e7c2aad9ca2023de9ec6194f301290b3
Camilo Valderrama <camilovalderrama@gmail.com>
2022-03-16 17:59:45 -0300

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=g295uOPgLZMhhWA9W2TVKfW61ILSYnVY`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+}
